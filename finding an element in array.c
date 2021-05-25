9d55acc0b70f66e6505cd34cc362ff45b51f069a
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-05-25 13:13:31 +0530

    Add files via upload

---
+++ finding an element in array.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	int i,arr[50],size,find,s=0;
+	printf("enter the size of array:");
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("enter the element you want to find:");
+	scanf("%d",&find);
+	for(i=0;i<size;i++)
+	{
+		if(find==arr[i])
+		{
+			s=1;
+			printf("element found");
+			break;
+		}
+		else
+		{
+			printf("element not found");
+			break;
+		}
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
