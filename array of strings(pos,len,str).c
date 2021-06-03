f7cd1c0ebd849c8d8cf228b8dcddbdca970b77ce
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-06-03 21:36:06 +0530

    Add files via upload

---
+++ array of strings(pos,len,str).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[100][100];
+	int i,maxpos,size;
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		scanf("%[^\n]s",&str[i]);
+	}
+	maxpos=0;
+	for(i=0;i<size;i++)
+	{
+	    if(strlen(str[i])>strlen(str[maxpos]))
+		maxpos=i;	
+	}
+	printf("%d %s %d",maxpos,str[maxpos],strlen(str[maxpos]));
+	return 0;
+}
